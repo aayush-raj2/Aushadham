66a8b86c00ab9d876b85b9f435b420e43ad9b838
Aayush Raj <aayushr679@gmail.com>
2025-10-20 15:57:33 +0530

    Add AppTest class for Spring context loading test

---
+++ Aushadham-backend/src/test/java/com/Aushadham/AppTest.java
@@ -0,0 +1,13 @@
+package com.aushadham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppTest {
+    
+    @Test
+    public void contextLoads() {
+        // Test that the Spring context loads successfully
+    }
+}
